85efd819765540d09ddf15ea0f0a6122047ed5c0
Kiss Judit <kijudit77@gmail.com>
2023-11-28 21:24:57 +0100

    largest product calculator

--- largest product in series/src/LargestProductCalculatorTest.java
+++ largest product in series/src/LargestProductCalculatorTest.java
@@ -1,21 +1,25 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LargestProductCalculatorTest {
     @Test
     void LargestProductCalculatorTest() {
-        Assertions.assertEquals(20, LargestProductCalculator.getLargestProduct("602144509", 2));
-        Assertions.assertEquals(80, LargestProductCalculator.getLargestProduct("602144509", 4));
-        Assertions.assertEquals(160, LargestProductCalculator.getLargestProduct("602144509", 5));
-        Assertions.assertEquals(9, LargestProductCalculator.getLargestProduct("602144509", 1));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("602144509", 15));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("602144509", 0));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("", 1));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("12345", 0));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("12345", 6));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct("", 5));
-        Assertions.assertEquals(504, LargestProductCalculator.getLargestProduct("123456789", 3));
-        Assertions.assertEquals(-1, LargestProductCalculator.getLargestProduct(null, 5));
-        Assertions.assertEquals(378, LargestProductCalculator.getLargestProduct("3497653109", 3));
+        assertEquals(20, LargestProductCalculator.getLargestProduct("602144509", 2));
+        assertEquals(80, LargestProductCalculator.getLargestProduct("602144509", 4));
+        assertEquals(160, LargestProductCalculator.getLargestProduct("602144509", 5));
+        assertEquals(9, LargestProductCalculator.getLargestProduct("602144509", 1));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("602144509", 15));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("602144509", 0));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("", 1));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("12345", 0));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("12345", 6));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct("", 5));
+        assertEquals(504, LargestProductCalculator.getLargestProduct("123456789", 3));
+        assertEquals(-1, LargestProductCalculator.getLargestProduct(null, 5));
+        assertEquals(378, LargestProductCalculator.getLargestProduct("3497653109", 3));
     }
+   
 }
