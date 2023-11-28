ce0ca243d13f80e529501f75dd751284a12cb928
Kiss Judit <kijudit77@gmail.com>
2023-11-28 21:50:20 +0100

    2.vizsgagyakorló első rész

--- 2.vizsgagyakorlo elso resz/src/ArmstrongNumber.java
+++ 2.vizsgagyakorlo elso resz/src/ArmstrongNumber.java
@@ -5,15 +5,15 @@ public class ArmstrongNumber {
         int armstrongSum = calculateArmstrongSum();
         System.out.println("The sum of Armstrong numbers between 1 and 10000 is " + armstrongSum);
         int primeSum = calculatePrimeSum();
-        System.out.println(primeSum);
+        System.out.println("The sum of primes is " + primeSum);
         double ratio = (double) primeSum / armstrongSum;
-        System.out.println(ratio);
+        System.out.println("The ratio is " + ratio);
     }
 
     private static int calculatePrimeSum() {
         int sum = 0;
-        for (int i = 2; i <= 10000 ; i++) {
-            if(isPrime(i)){
+        for (int i = 2; i <= 10000; i++) {
+            if (isPrime(i)) {
                 sum += i;
             }
         }
@@ -21,11 +21,11 @@ public class ArmstrongNumber {
     }
 
     private static boolean isPrime(int number) {
-        if(number <= 1){
+        if (number <= 1) {
             return false;
         }
-        for (int i = 2; i < Math.sqrt(number) ; i++) {
-            if(number % i == 0){
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % i == 0) {
                 return false;
             }
         }
@@ -45,17 +45,12 @@ public class ArmstrongNumber {
     private static boolean isArmstrong(int number) {
         int originalNumber, remainder, result = 0, n = 0;
         originalNumber = number;
-        for (; originalNumber != 0; originalNumber /= 10, ++n) {
-            originalNumber = number;
-        }
+        for (originalNumber = number; originalNumber != 0; originalNumber /= 10, ++n) ;
+        originalNumber = number;
         for (; originalNumber != 0; originalNumber /= 10) {
             remainder = originalNumber % 10;
             result += Math.pow(remainder, n);
         }
-        if (result == number) {
-            return true;
-        } else {
-            return false;
-        }
+        return result == number;
     }
 }
--- 2.vizsgagyakorlo elso resz/src/Erastothenes.java
+++ 2.vizsgagyakorlo elso resz/src/Erastothenes.java
@@ -1,19 +1,27 @@
 public class Erastothenes {
     public static void main(String[] args) {
         int limit = 1000;
-       // int[] primes = generatePrimes(limit);
-       // int sum = calculateSumOfPrimes(primes);
-       // System.out.println("The sum of the first 1000 primes is: " + sum);
+        int[] primes = generatePrimes(limit);
+        int sum = calculateSumOfPrimes(primes);
+        System.out.println("The sum of the first 1000 primes is: " + sum);
     }
 
-    private static int[] calculateSumOfPrimes(int number) {
-        boolean[] isPrime = new boolean[number + 1];
-        for (int i = 2; i <= number; i++) {
+    private static int calculateSumOfPrimes(int[] primes) {
+        int sum = 0;
+        for (int prime : primes) {
+            sum += prime;
+        }
+        return sum;
+    }
+
+    private static int[] generatePrimes(int limit) {
+        boolean[] isPrime = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++) {
             isPrime[i] = true;
         }
-        for (int i = 2; i * i <= number; i++) {
+        for (int i = 2; i * i <= limit; i++) {
             if (isPrime[i]) {
-                for (int j = i * i; j <= number; j += i) {
+                for (int j = i * i; j <= limit; j += i) {
                     isPrime[j] = false;
                 }
             }
@@ -26,19 +34,11 @@ public class Erastothenes {
         }
         int[] primes = new int[counter];
         int index = 0;
-        for (int i = 2; i <= number; i++) {
+        for (int i = 2; i <= limit; i++) {
             if (isPrime[i]) {
                 primes[index++] = i;
             }
         }
         return primes;
-    }
-
-    private static int generatePrimes(int[] numbers) {
-        int sum = 0;
-        for (int number : numbers) {
-            sum += number;
-        }
-        return sum;
     }
 }
