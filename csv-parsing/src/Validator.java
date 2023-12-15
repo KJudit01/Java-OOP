4d70b58943e0ecf81e627e31ae68319de8e235dc
Kiss Judit <kijudit77@gmail.com>
2023-12-15 19:34:45 +0100

    csv-parsing modified

--- csv-parsing/src/Validator.java
+++ csv-parsing/src/Validator.java
@@ -1,5 +1,9 @@
+import java.util.regex.Pattern;
+
 public class Validator {
+    private static final Pattern ALLOWED_CHARS_PATTERN = Pattern.compile("^[A-Za-z0-9 .,]+$");
+
     public boolean isSafe(String input) {
-        return !input.toLowerCase().contains("<script>") && !input.toLowerCase().contains("javascript:");
+        return ALLOWED_CHARS_PATTERN.matcher(input).matches();
     }
 }
